0b650aa5b00427df8670012d4eac86a5bc6f5519
Connor Clark <connor.clark@coursehero.com>
2018-08-01 17:09:15 -0700

    dev server

--- src/mythos-app/index.jsx
+++ src/mythos-app/index.jsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal'
 import React from 'react'
+import '../register'
 
 const [test, a] = [1,2]
 
@@ -11,5 +12,14 @@ const MythosApp = () => (
     </Modal>
   </div>
 )
+
+if (module.hot) {
+  module.hot.accept()
+  if (window.ComponentLibraries['@coursehero/mythos'].MythosApp) {
+    window.ComponentLibraries['@coursehero/mythos'].MythosApp.HotReload(
+      MythosApp,
+    )
+  }
+}
 
 export default MythosApp
